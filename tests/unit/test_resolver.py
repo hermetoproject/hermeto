hermetoproject/hermeto

commit d4d8dd9ad3878fb963b5ab97faf1168ea1520d03

Use temporary directory when resolving each package manager
Closes https://github.com/hermetoproject/cachi2/issues/707
Closes https://github.com/hermetoproject/cachi2/issues/712

All package managers (except yarn) work on top of the input source
directory directly which may result in dirtying the repo which is
completely undesirable.

The changes simplify the package resolution logic by removing the
conditional check for yarn_packages. This ensures that the temporary
directory is always used, making the behavior consistent across all
package types. The test suite has also been updated to reflect this
change.

This refactor aligns with the goal of maintaining a uniform approach to
handling source directories, regardless of the package manager being
used.

Signed-off-by: Michal Šoltis <msoltis@redhat.com>

cachi2/core/resolver.py
@@ -39,26 +39,22 @@ def resolve_packages(request: Request) -> RequestOutput:
     This function performs the operations in a working copy of the source directory in case
     a package manager that can make unwanted modifications will be used.
     """
-    if request.yarn_packages:
-        original_source_dir = request.source_dir
+    original_source_dir = request.source_dir
 
-        with TemporaryDirectory(".cachi2-source-copy", dir=".") as temp_dir:
-            source_backup = copy_directory(original_source_dir.path, Path(temp_dir).resolve())
+    with TemporaryDirectory(".cachi2-source-copy", dir=".") as temp_dir:
+        source_backup = copy_directory(original_source_dir.path, Path(temp_dir).resolve())
 
-            request.source_dir = RootedPath(source_backup)
-            output = _resolve_packages(request)
-            request.source_dir = original_source_dir
+        request.source_dir = RootedPath(source_backup)
+        output = _resolve_packages(request)
+        request.source_dir = original_source_dir
 
-            # Temporary solution to project files paths that are pointing to the work copy.
-            # Should be replaced once we extend the work copy solution to other package managers.
-            # https://github.com/hermetoproject/cachi2/issues/712
-            for project_file in output.build_config.project_files:
+        for project_file in output.build_config.project_files:
+            # if the project file is in the output directory, we don't need to update the path
+            if not project_file.abspath.is_relative_to(request.output_dir):
                 subpath = project_file.abspath.relative_to(source_backup)
                 project_file.abspath = original_source_dir / subpath
 
-            return output
-    else:
-        return _resolve_packages(request)
+        return output
 
 
 def _resolve_packages(request: Request) -> RequestOutput:

tests/unit/test_resolver.py
@@ -1,6 +1,5 @@
 import re
 from pathlib import Path
-from typing import Callable
 from unittest import mock
 
 import pytest
@@ -70,50 +69,40 @@ COMBINED_OUTPUT = RequestOutput.from_obj_list(
 )
 
 
-def test_resolve_packages(tmp_path: Path) -> None:
+@mock.patch("cachi2.core.resolver._resolve_packages")
+def test_resolve_packages_updates_project_files(
+    mock_resolve_packages: mock.Mock, tmp_path: Path
+) -> None:
     request = Request(
         source_dir=tmp_path,
         output_dir=tmp_path,
         packages=[{"type": "pip"}, {"type": "npm"}, {"type": "gomod"}],
     )
 
-    calls_by_pkgtype = []
+    def fake_resolve_packages(request: Request) -> RequestOutput:
+        output = COMBINED_OUTPUT
+        for project_file in output.build_config.project_files:
+            project_file.abspath = request.source_dir.path / project_file.abspath.name
 
-    def mock_fetch(pkgtype: str, output: RequestOutput) -> Callable[[Request], RequestOutput]:
-        def fetch(req: Request) -> RequestOutput:
-            assert req == request
-            calls_by_pkgtype.append(pkgtype)
-            return output
+        return output
 
-        return fetch
+    mock_resolve_packages.side_effect = fake_resolve_packages
 
-    with mock.patch.dict(
-        resolver._package_managers,
-        {
-            "gomod": mock_fetch("gomod", GOMOD_OUTPUT),
-            "npm": mock_fetch("npm", NPM_OUTPUT),
-            "pip": mock_fetch("pip", PIP_OUTPUT),
-        },
-    ):
-        assert resolver.resolve_packages(request) == COMBINED_OUTPUT
-
-    assert calls_by_pkgtype == ["gomod", "npm", "pip"]
+    assert resolver.resolve_packages(request) == COMBINED_OUTPUT
+    assert request.source_dir == RootedPath(tmp_path)
 
 
 @pytest.mark.parametrize(
-    "packages, copy_exists",
+    "packages",
     [
-        pytest.param([{"type": "yarn"}], True, id="single_package"),
-        pytest.param(
-            [{"type": "gomod"}, {"type": "pip"}, {"type": "npm"}], False, id="multiple_packages"
-        ),
+        pytest.param([{"type": "yarn"}], id="single_package"),
+        pytest.param([{"type": "gomod"}, {"type": "pip"}, {"type": "npm"}], id="multiple_packages"),
     ],
 )
 @mock.patch("cachi2.core.resolver._resolve_packages")
 def test_source_dir_copy(
     mock_resolve_packages: mock.Mock,
     packages: list[dict[str, str]],
-    copy_exists: bool,
     tmp_path: Path,
 ) -> None:
     request = Request(
@@ -123,16 +112,12 @@ def test_source_dir_copy(
     )
 
     def _resolve_packages(request: Request) -> RequestOutput:
-        if copy_exists:
-            tmp_dir_name = request.source_dir.path.name
+        tmp_dir_name = request.source_dir.path.name
 
-            # assert a temporary directory is being used
-            assert tmp_dir_name != tmp_path.name
-            assert tmp_dir_name.startswith("tmp")
-            assert tmp_dir_name.endswith(".cachi2-source-copy")
-        else:
-            # assert the original source_dir is being used
-            assert request.source_dir == RootedPath(tmp_path)
+        # assert a temporary directory is being used
+        assert tmp_dir_name != tmp_path.name
+        assert tmp_dir_name.startswith("tmp")
+        assert tmp_dir_name.endswith(".cachi2-source-copy")
 
         return RequestOutput.empty()
 

commit 71f3ce880a22b56cab57d4709861e312440303e7

core: resolver: Fix how project files paths are updated
We have essentially 2 scenarios we need to cover:
1) Bundler
    - we do need to override user configuration with a file, but unlike
      other backends (i.e. pip) we can put the override file to our
      output dir and reference it from there
    - we must not try updating the project file path because it's
      already properly resolved

2) Pip/NPM
    - these require changes to project files directly inside the source
      repository and so we must update the paths correctly
    - since our default working directory is now temporary, the project
      files paths' are resolved within that and we need to fix them
    - since the default working directory is inside the source
      repository, it's easy because the project file template is always
      going to be a relative subpath of the other and we can safely
      replace

The problem appears when the stars align and the output dir, the source
dir, and CWD are all identical (yes, this happened). With our current
logic we'd process Bundler just fine, because that one places its
project file override to the output dir directly and our temporary
working copy (i.e. <tmp>.cachi2-source-copy) directory isn't a subpath
of it. However, Pip/NPM backends need to place their overrides back to
the original source repo and that's where we'd cause a hermetic build
to fail because the temporary working copy is a subpath of the absolute
path for the project file residing in the original source directory and
so we wouldn't update because it's "seemingly" already there. By
forcing and incorrect path for the project file override the subsequent
hermetic build would fail because the file wouldn't be in its expected
location either by Pip or NPM and either backend would instead try to
connect to the internet.

This patch forces the replacement of all paths. If the replacement
fails we skip such path. This should cover all cases because our
temporary working copy is always within the original source path with
case 3 being the only determinant (at this point in time) of the
outcome.
This patch also re-enables the resolver project path mangling unit test
since now the issue is gone and the test verifies it.

Fixes: d4d8dd9ad3878fb963b5ab97faf1168ea1520d03

cachi2/core/resolver.py
@@ -48,11 +48,15 @@ def resolve_packages(request: Request) -> RequestOutput:
         output = _resolve_packages(request)
         request.source_dir = original_source_dir
 
+        # Update all project file paths that end up directly in the source repository
         for project_file in output.build_config.project_files:
-            # if the project file is in the output directory, we don't need to update the path
-            if not project_file.abspath.is_relative_to(request.output_dir):
+            try:
                 subpath = project_file.abspath.relative_to(source_backup)
                 project_file.abspath = original_source_dir / subpath
+            except ValueError:
+                # '<project_file.abspath> is not in the subpath of <source_backup>', i.e the file
+                # is referenced directly from the output directory and doesn't need replacing
+                continue
 
         return output
 

tests/unit/test_resolver.py
@@ -129,11 +129,18 @@ def test_source_dir_copy(
     assert request.source_dir == RootedPath(tmp_path)
 
 
+@pytest.mark.parametrize(
+    "with_path_replacement",
+    (
+        pytest.param(True, id="with_path_replacement"),
+        pytest.param(False, id="without_path_replacement"),
+    ),
+)
 @mock.patch("cachi2.core.resolver._resolve_packages")
-@pytest.mark.skip(reason="Temporary - HEAD is broken and the test would uncover it")
 def test_project_files_fix_for_work_copy(
     mock_resolve_packages: mock.Mock,
     tmp_path: Path,
+    with_path_replacement: bool,
 ) -> None:
     request = Request(
         source_dir=tmp_path,
@@ -146,13 +153,13 @@ def test_project_files_fix_for_work_copy(
         assert request.source_dir.path != tmp_path
         assert request.source_dir.path.name.endswith(".cachi2-source-copy")
 
-        # return a project file pointing to the source dir copy
+        abspath = request.source_dir.path if with_path_replacement else request.output_dir.path
         return RequestOutput(
             components=[],
             build_config=BuildConfig(
                 environment_variables=[],
                 project_files=[
-                    ProjectFile(abspath=request.source_dir.path / "package.json", template="n/a"),
+                    ProjectFile(abspath=abspath / "package.json", template="n/a"),
                 ],
             ),
         )