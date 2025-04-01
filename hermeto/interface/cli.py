hermetoproject/hermeto

commit 57b9871f9e21327bfbacfac92b64614622ecf001

interface: cli: Extract fetch-deps/merge-sboms doc strings to constants
Future patch will convert these strings to dynamically formatted ones
using f-strings based on the entrypoint that was used for the
application. Usage of f-strings in docstrings is discouraged, however,
flake8 doesn't really have a builtin rule for this (as there is no PEP)
for it, instead, it crashes when parsing the code:

    File "/usr/local/lib/python3.9/ast.py", line 71, in _convert_num
        _raise_malformed_node(node)
    File "/usr/local/lib/python3.9/ast.py", line 68, in _raise_malformed_node
        raise ValueError(f'malformed node or string: {node!r}')
    ValueError: malformed node or string: <ast.JoinedStr object at 0x7fcf81ad5580>

The proper fix here is to not rely on help messages for these commands
to be generated out of doc strings automatically, but supply them
as a parameter explicitly via the command decorator.

Take the opportunity and perform a tiny cosmetic formatting change to
the merge-sboms message by inserting \b backspace escapes to prevent
click's default text rewrapping [1].

[1] https://click.palletsprojects.com/en/stable/documentation/#preventing-rewrapping

Signed-off-by: Erik Skultety <eskultet@redhat.com>

hermeto/interface/cli.py
@@ -27,6 +27,59 @@ log = logging.getLogger(__name__)
 DEFAULT_SOURCE = "."
 DEFAULT_OUTPUT = "./cachi2-output"
 
+FETCH_DEPS_HELP = """\
+    fetch dependencies for supported package managers.
+
+    \b
+    # gomod package in the current directory
+    cachi2 fetch-deps gomod
+
+    \b
+    # pip package in the root of the source directory
+    cachi2 fetch-deps --source ./my-repo pip
+
+    \b
+    # gomod package in a subpath of the source directory (./my-repo/subpath)
+    cachi2 fetch-deps --source ./my-repo '{
+        "type": "gomod",
+        "path": "subpath"
+    }'
+
+    \b
+    # multiple packages as a json list
+    cachi2 fetch-deps '[
+        {"type": "gomod"},
+        {"type": "gomod", "path": "subpath"},
+        {"type": "pip", "path": "other-path"}
+    ]'
+
+    \b
+    # multiple packages and flags as a json list
+    cachi2 fetch-deps '{
+        "packages": [
+            {"type": "gomod"},
+            {"type": "gomod", "path": "subpath"},
+            {"type": "pip", "path": "other-path"}
+        ],
+        "flags": [
+            "gomod-vendor"
+        ]
+    }'
+    """
+
+MERGE_SBOMS_HELP = """\
+        Merge two or more SBOMs into one.
+
+        \b
+        The command works with SBOMs generated by our application and with a supported subset of
+        SPDX SBOMs. You might want to run
+
+            \b
+            cachi2 fetch-deps <args...>
+
+        \b
+        first to produce SBOMs to merge.
+        """
 
 OUTFILE_OPTION = typer.Option(
     None,
@@ -144,7 +197,7 @@ class _Input(pydantic.BaseModel, extra="forbid"):
     flags: list[Flag] = list()
 
 
-@app.command()
+@app.command(help=FETCH_DEPS_HELP)
 @handle_errors
 def fetch_deps(
     raw_input: str = typer.Argument(
@@ -198,44 +251,7 @@ def fetch_deps(
     ),
     sbom_type: SBOMFormat = SBOM_TYPE_OPTION,
 ) -> None:
-    """Fetch dependencies for supported package managers.
-
-    \b
-    # gomod package in the current directory
-    cachi2 fetch-deps gomod
-
-    \b
-    # pip package in the root of the source directory
-    cachi2 fetch-deps --source ./my-repo pip
-
-    \b
-    # gomod package in a subpath of the source directory (./my-repo/subpath)
-    cachi2 fetch-deps --source ./my-repo '{
-        "type": "gomod",
-        "path": "subpath"
-    }'
-
-    \b
-    # multiple packages as a JSON list
-    cachi2 fetch-deps '[
-        {"type": "gomod"},
-        {"type": "gomod", "path": "subpath"},
-        {"type": "pip", "path": "other-path"}
-    ]'
-
-    \b
-    # multiple packages and flags as a JSON list
-    cachi2 fetch-deps '{
-        "packages": [
-            {"type": "gomod"},
-            {"type": "gomod", "path": "subpath"},
-            {"type": "pip", "path": "other-path"}
-        ],
-        "flags": [
-            "gomod-vendor"
-        ]
-    }'
-    """  # noqa: D301, D202; backslashes intentional, blank line required by black
+    """Fetch dependencies for supported package managers."""
 
     def normalize_input() -> dict[str, list[Any]]:
         """Format raw_input so it can be parsed by the _Input class."""
@@ -406,7 +422,7 @@ def _prevalidate_sbom_files_args(sbom_files_to_merge: Paths) -> Paths:
     return all_files_are_jsons(enough_files_for_merge(list(set(sbom_files_to_merge))))
 
 
-@app.command()
+@app.command(help=MERGE_SBOMS_HELP)
 @handle_errors
 def merge_sboms(
     sbom_files_to_merge: Paths = typer.Argument(
@@ -425,15 +441,7 @@ def merge_sboms(
         None, "--sbom-name", help="Name of the resulting merged SBOM."
     ),
 ) -> None:
-    """Merge two or more SBOMs into one.
-
-    The command works with SBOMs generated by our application and with a supported subset of
-    SPDX SBOMs. You might want to run
-
-        cachi2 fetch-deps <args...>
-
-    first to produce SBOMs to merge.
-    """
+    """Merge two or more SBOMs into one."""
     sboms_to_merge: List[Union[SPDXSbom, Sbom]] = []
     for sbom_file in sbom_files_to_merge:
         sbom_dict = json.loads(sbom_file.read_text())

commit e9c2686623e43021251c5440cb57d7bd7baa63a2

cli: Fix help message doc strings constants missing f-string markers
Our CLI help messages currently report the generic APP_NAME placeholder
instead of the actual entrypoint:

    $ hermeto fetch-deps --help
     Usage: hermeto fetch-deps [OPTIONS] PKG

     fetch dependencies for supported package managers.
     # gomod package in the current directory
     {APP_NAME} fetch-deps gomod
    ...

Though unrelated, take the opportunity to fix the casing at the
beginning of the sentence at the same time.

fixes: 57b9871f9e21327bfbacfac92b64614622ecf001

Signed-off-by: Erik Skultety <eskultet@redhat.com>

hermeto/interface/cli.py
@@ -28,8 +28,8 @@ log = logging.getLogger(__name__)
 DEFAULT_SOURCE = "."
 DEFAULT_OUTPUT = f"./{APP_NAME}-output"
 
-FETCH_DEPS_HELP = """\
-    fetch dependencies for supported package managers.
+FETCH_DEPS_HELP = f"""\
+    Fetch dependencies for supported package managers.
 
     \b
     # gomod package in the current directory
@@ -68,7 +68,7 @@ FETCH_DEPS_HELP = """\
     }}'
     """
 
-MERGE_SBOMS_HELP = """\
+MERGE_SBOMS_HELP = f"""\
         Merge two or more SBOMs into one.
 
         \b