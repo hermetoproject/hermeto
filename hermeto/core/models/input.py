63ee04242abbaba543ab0fb3ae50b33cb73bc767
Vladimir Aleksandrov <valeksan@redhat.com>
2025-06-30 08:49:45 +0200

    Allow experimental features to be toggled individually
    Allows resolution of experimental x-<pkg> types of package managers while hiding the normal package manager name and providing helpfull errors.
    
    Later should replace dev-package-managers, for now both are working.
    
    Closes: https://github.com/hermetoproject/hermeto/issues/687
    
    Signed-off-by: Vladimir Aleksandrov <valeksan@redhat.com>

--- hermeto/core/models/input.py
+++ hermeto/core/models/input.py
@@ -35,6 +35,7 @@ def _present_user_input_error(validation_error: pydantic.ValidationError) -> str
     Compared to pydantic's default message:
     - don't show the model name, just say "user input"
     - don't show the underlying error type (e.g. "type=value_error.const")
+    - don't show experimental package types without x- prefix in the error message
     """
     errors = validation_error.errors()
     n_errors = len(errors)
@@ -42,6 +43,22 @@ def _present_user_input_error(validation_error: pydantic.ValidationError) -> str
     def show_error(error: "ErrorDict") -> str:
         location = " -> ".join(map(str, error["loc"]))
         message = error["msg"]
+
+        if "expected tags:" in message:
+            tags = message.split("expected tags:")[1].strip()
+            tags = [tag.strip("'") for tag in tags.split(", ")]
+            # For each tag, if there's an experimental version, use that instead
+            filtered_tags = []
+            for tag in tags:
+                if tag.startswith("x-"):
+                    filtered_tags.append(tag)
+                elif f"x-{tag}" not in tags:
+                    filtered_tags.append(tag)
+            message = (
+                message.split("expected tags:")[0]
+                + "expected tags: "
+                + ", ".join(f"'{tag}'" for tag in sorted(filtered_tags))
+            )
 
         if location != "__root__":
             message = f"{location}\n  {message}"
